10db8a2db1cf305ae22b7d14a8cd36181613434d
Noel Rincón <72935460+NOEL318@users.noreply.github.com>
2023-04-12 14:30:27 -0600

    Pets-Store .NET
    .NET project initialized client API

---
+++ Pets-Store/Controllers/ClientController.cs
@@ -0,0 +1,91 @@
+﻿using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pets_Store.Models;
+
+namespace Pets_Store.Controllers
+{
+	[ApiController]
+	[Route("client")]
+	public class ClientController : ControllerBase
+	{
+		[HttpGet]
+		[Route("list")]
+		public dynamic ClientsList()
+		{
+
+			List<Client> clients = new List<Client>
+			{
+				new Client
+				{
+					id = "1",
+					name = "Noel",
+					age = "18",
+					email = "noelrinaya318@gmail.com",
+
+				}
+
+			};
+			return clients;
+		}
+
+
+		[HttpPost]
+		[Route("save")]
+		public dynamic SaveClient(Client client)
+		{
+
+			client.id = "3";
+			return new
+			{
+				success = true,
+				message = "Succesfully Saved",
+				result = client
+			};
+		}
+
+		[HttpGet]
+		[Route("listbyid")]
+		public dynamic ListClientById(string _id)
+		{
+
+
+			return new Client
+			{
+				id = _id,
+				name = "Noel",
+				age = "18",
+				email = "noelrinaya318@gmail.com",
+
+			};
+		}
+
+		[HttpPost]
+		[Route("delete")]
+		public dynamic DeleteClient(Client client)
+		{
+			string token = Request.Headers.Where(x => x.Key == "Authorization").FirstOrDefault().Value;
+			if (token != "aBscd73718")
+			{
+				return new
+				{
+					success = false,
+					message = "Invalid Token/Credentials",
+					result = "",
+				};
+			}
+			else
+			{
+				return new
+				{
+					success = true,
+					message = "Succesfully Deleted",
+					result = client,
+				};
+
+			}
+
+		}
+
+	}
+}
---
+++ Pets-Store/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Pets_Store.Models
+{
+	public class Client
+	{
+		public string id { get; set;}
+        public string name { get; set; }
+        public string email { get; set; }
+        public string age { get; set; }
+
+    }
+}
+
